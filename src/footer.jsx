87f891e87ac01c20c798434b9d4f3edcd4d9db67
jhossain3 <jannah.hossain@kpmg.co.uk>
2022-05-12 14:05:26 +0100

    minor file edits

---
+++ src/footer.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Box from '@mui/material/Box';
+
+export default function Footer() {
+  return (
+    <>
+      <Container disableGutters maxWidth={false}>
+      <Box  sx={{ bgcolor: ' #000000', height: '25px', width:'100%'}} />
+      </Container>
+    </>
+  );
+}
