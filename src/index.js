b53e92a820ee9a68e3a01ab19d479ed6e9427054
jhossain3 <jannah.hossain@kpmg.co.uk>
2022-05-24 15:31:33 +0100

    made page and route to page

--- src/index.js
+++ src/index.js
@@ -1,40 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './homePage';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./homePage";
+import reportWebVitals from "./reportWebVitals";
 import { GlobalStyles } from "@mui/material";
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme';
-import Footer from './footer';
-import Discover from './discoverPlaces';
-import Japan from './japan'
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+import Footer from "./footer";
+import Discover from "./discoverPlaces";
+import Japan from "./japan";
+import Create from "./create";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles
       styles={{
         body: { backgroundColor: "#E6EFD1" },
-          }}
-          />
-  <React.StrictMode>
-  <BrowserRouter>
-      <Routes>
-        <Route path="/"  element={<Home />}>
-        
-        </Route>
-        <Route path="/discover" element={<Discover />}>
-
-        </Route>
-        <Route path="/japan" element={<Japan />}>
-
-</Route>
-      </Routes>
-    </BrowserRouter>
-    <Footer/>
-  </React.StrictMode>
-
+      }}
+    />
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/discover" element={<Discover />}></Route>
+          <Route path="/japan" element={<Japan />}></Route>
+          <Route path="/create" element={<Create />}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </React.StrictMode>
   </ThemeProvider>
 );
 
