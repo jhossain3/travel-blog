cdc6407ac2d12c1811c3cd5f5488dea8877f37e1
jhossain3 <jannah.hossain@kpmg.co.uk>
2022-06-01 14:23:12 +0100

    very basic form on create pg plus formatting changes to hmpg

---
+++ src/ImageUpload.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UploadIcon from "@mui/icons-material/Upload";
+import IconButton from '@material-ui/core/IconButton';
+
+
+export default function ImageUploadCard() {
+
+  return (
+    <><IconButton color="primary">
+      <UploadIcon fontSize="large" />
+    </IconButton></>
+  );
+}
