3109e1d763f560a78f5f97fdb886615e44caf1ef
jhossain3 <jannah.hossain@kpmg.co.uk>
2022-06-30 15:35:54 +0100

    backend database connection
    this is setup for mongodb database with authors collection
    this posts data with express to url /getAuthors
    then the front end fetches the authors collection (data) from this url and uses it in the create page
    create page is populating dropdown of authors with this authors data

--- src/create.jsx
+++ src/create.jsx
@@ -1,29 +1,50 @@
 import * as React from "react";
 import {
   Box,
+  Button,
   Grid,
   TextField,
   Typography,
   FormControl,
+  MenuList,
   MenuItem,
   InputLabel,
+  OutlinedInput,
   Select,
 } from "@mui/material";
 import ImageUploadCard from "./ImageUpload";
 
-export default function Create() {
-  const [author, setAuthor] = React.useState("");
+const names = [];
 
-// const getAuthors = ()=>{
-//   axios.get("localhost:3003/getAuthors")
-//   .then((res)=>{
-//     console.log(res);
-//   })
-// }
+async function getAuthors() {
+
+  const res = await fetch("http://localhost:3003/getAuthors");
+  const resData = await res.json();
+  console.log(resData);
+  for (let i = 0; i < resData.length; i++) {
+    names[i] = resData[i].name;
+  }
+  console.log(names);
+
+  return names;
+}
+
+getAuthors();
+
+export default function Create() {
+
+  const [personName, setPersonName] = React.useState([]);
 
   const handleChange = (event) => {
-    setAuthor(event.target.value);
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
   };
+
   return (
     <Grid
       container
@@ -62,8 +83,8 @@ export default function Create() {
               sx={{
                 pb: "30px",
               }}
-              id="names"
-              label="Names"
+              id="whoCame"
+              label="who came"
             />
             <TextField
               sx={{
@@ -75,24 +96,31 @@ export default function Create() {
             <ImageUploadCard />
             <Typography sx={{ pb: "25px" }}> Pictures </Typography>
             <Box sx={{ minWidth: 120 }}>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Author</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={author}
-                  label="Author"
-                  onChange={handleChange}
-                >
-                  <MenuItem value={"Sadiyah"}>Sadiyah</MenuItem>
-                  <MenuItem value={"Jannah"}>Jannah</MenuItem>
-                  <MenuItem value={"Yasmin"}>Yasmin</MenuItem>
-                  <MenuItem value={"Jessica"}>Jessica</MenuItem>
-                  <MenuItem value={"Adina"}>Adina</MenuItem>
-                  <MenuItem value={"Aysha"}>Aysha</MenuItem>
-                </Select>
-              </FormControl>
+            <div>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id="demo-multiple-name-label">Name</InputLabel>
+        <Select
+          labelId="demo-multiple-name-label"
+          id="demo-multiple-name"
+          multiple
+          value={personName}
+          onChange={handleChange}
+          input={<OutlinedInput label="Name" />}
+
+        >
+          {names.map((name) => (
+            <MenuItem
+              key={name}
+              value={name}
+            >
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
             </Box>
+            <Button>Hello</Button>
           </Grid>
         </Box>
       </Grid>
