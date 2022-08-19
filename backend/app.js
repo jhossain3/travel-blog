47f4c82e4537609e6e915c5240eccf822822fb0b
jhossain3 <jannah.hossain@kpmg.co.uk>
2022-08-19 17:05:30 +0100

    dropdown populated from mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,35 @@
 const express = require("express");
 const db = require("./data/database");
-
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE" // what matters here is that OPTIONS is present
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
-try {
+
   app.get("/getAuthors", async function (req, res) {
+    try{
+          console.log("hi");
     db.connectToDatabase();
     var authorArray = [];
     authorArray = await db.getDb().collection("authors").find().toArray();
     console.log(JSON.stringify(authorArray));
     res.send(authorArray);
+    
+    }
+    catch(e){
+      console.log(e);
+    }
+
     // res.render('create', { authorArray: authorArray });
   });
-} catch (e) {
-  console.log(e);
-}
+
 
 // try {
 //   app.post("/discover", async function(req, res){
@@ -41,7 +51,8 @@ try {
 // }
 
 app.get("/", function (req, res) {
-  res.send("Home");
+  res.send("Home2");
+  console.log(db.connectToDatabase());
 });
 
 app.listen(3003);
