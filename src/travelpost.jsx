7b6a2860b7acf05b07664345cf1e858741b9bb06
jhossain3 <jannah.hossain@kpmg.co.uk>
2022-11-14 20:52:58 +0000

    fixed it

--- src/travelpost.jsx
+++ src/travelpost.jsx
@@ -16,7 +16,6 @@ export default function TravelPost({ match }) {
   const [post, setPost] = useState({author: {id:'', name:'', email:''}, pictures: '', dateNow:'', checkIn: '',flightHours: '', groupSize: '', checkOut: '', destination:'', _id:'', summary: ''});
   
   const { id } = useParams();
-  console.log(id);
 
   const travelImage =
     "https://images.unsplash.com/photo-1480796927426-f609979314bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8amFwYW58ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60";
@@ -53,32 +52,16 @@ export default function TravelPost({ match }) {
           variant="subtitle1"
           sx={{ display: "flex", justifyContent: "flex-end" }}
         >
-         {post.dateNow} by {post.author.name}
+         {post.dateNow.slice(0,10)}
+         
         </Typography>
+        <Typography
+          variant="subtitle1"
+          sx={{ display: "flex", justifyContent: "flex-end" }}
+        > {post.author.name}</Typography>
 
         <Typography sx={{ mt: "20px" }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Gravida
-          neque convallis a cras semper auctor neque. Scelerisque fermentum dui
-          faucibus in ornare quam viverra. Cursus sit amet dictum sit amet justo
-          donec enim. Nam aliquam sem et tortor consequat id. Libero enim sed
-          faucibus turpis in eu. Diam ut venenatis tellus in metus vulputate. Eu
-          feugiat pretium nibh ipsum consequat nisl vel pretium. Commodo
-          ullamcorper a lacus vestibulum sed. Aliquam id diam maecenas
-          ultricies. Donec adipiscing tristique risus nec feugiat in fermentum
-          posuere urna. Ut aliquam purus sit amet luctus. Metus vulputate eu
-          scelerisque felis imperdiet proin. Magnis dis parturient montes
-          nascetur. In pellentesque massa placerat duis ultricies lacus sed
-          turpis tincidunt. Ultrices vitae auctor eu augue ut lectus arcu
-          bibendum at. Commodo nulla facilisi nullam vehicula ipsum a arcu. Sit
-          amet luctus venenatis lectus magna fringilla. Nunc scelerisque viverra
-          mauris in aliquam sem fringilla ut morbi. Sed libero enim sed faucibus
-          turpis. Eget duis at tellus at urna condimentum mattis. A lacus
-          vestibulum sed arcu non. Ut enim blandit volutpat maecenas volutpat.
-          Justo donec enim diam vulputate ut pharetra. Sollicitudin tempor id eu
-          nisl nunc mi. Nullam eget felis eget nunc lobortis mattis aliquam
-          faucibus purus. Pellentesque elit eget gravida cum. Lectus proin nibh
-          nisl condimentum id venenatis a condimentum.
+          {post.summary}
         </Typography>
         <Grid
           container
@@ -96,7 +79,7 @@ export default function TravelPost({ match }) {
             }}
           >
             <FlightIcon sx={{ fontSize: "70px" }} />
-            <Typography variant ="h5" sx={{ ml: "20px" }}>4 hrs</Typography>
+            <Typography variant ="h5" sx={{ ml: "20px" }}>{post.flightHours} hrs</Typography>
           </Box>
           <Box
             item container
@@ -111,7 +94,7 @@ export default function TravelPost({ match }) {
             }}
           >
             <PersonIcon sx={{ fontSize: "70px" }} />
-            <Typography variant ="h5" sx={{ wordWrap: "break-word", ml: "20px" }}>5+</Typography>
+            <Typography variant ="h5" sx={{ wordWrap: "break-word", ml: "20px" }}>{post.groupSize}</Typography>
           </Box>
           <Box
             item
@@ -126,7 +109,7 @@ export default function TravelPost({ match }) {
             }}
           >
             <CalendarTodayIcon sx={{ fontSize: "70px" }} />
-            <Typography  sx={{ ml: "20px", fontSize: '15px'}}>{post.dates}</Typography>
+            <Typography  sx={{ ml: "20px", fontSize: '15px'}}>{post.checkIn.slice(0,10)} - {post.checkOut.slice(0,10)}</Typography>
           </Box>
         </Grid>
       </Grid>
